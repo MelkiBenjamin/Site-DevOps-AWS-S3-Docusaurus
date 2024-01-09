b74fa17a78ae806f65ff40e1b3a585210a37ca9b
benjamin melki <mbenjaminmelki@gmail.com>
2024-01-09 16:02:54 +0100

    dite devops test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/challenge-s10-e03-bis-MelkiBenjamin/',
+  baseUrl: '/site-devops/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
